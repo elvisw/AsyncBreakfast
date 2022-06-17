a62a045ee77364de463403422dc6911fd71b3af1
elvis_w <me@elvisw.com>
2022-06-17 10:34:52 +0800

    添加项目文件。

---
+++ AsyncBreakfast/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using AsyncBreakfast;
+
+Coffee cup = Cook.PourCoffee();
+Console.WriteLine("coffee is ready");
+
+Egg eggs = Cook.FryEggs(2);
+Console.WriteLine("eggs are ready");
+
+Bacon bacon = Cook.FryBacon(3);
+Console.WriteLine("bacon is ready");
+
+Toast toast = Cook.ToastBread(2);
+Cook.ApplyButter(toast);
+Cook.ApplyJam(toast);
+Console.WriteLine("toast is ready");
+
+Juice oj = Cook.PourOJ();
+Console.WriteLine("oj is ready");
+Console.WriteLine("Breakfast is ready!");
